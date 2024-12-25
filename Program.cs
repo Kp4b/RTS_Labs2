65913ea9f6f0b6b14a6fc77cc469c669c6eed730
Kp4b <tonalli2012@gmail.com>
2024-12-25 19:48:07 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // Request the size of the data files from the user
+        Console.Write("Enter the size of the data files in KB: ");
+        if (!int.TryParse(Console.ReadLine(), out int dataSizeInKB))
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+            return;
+        }
+
+        // Convert size from MB to number of double values
+        int dataSize = dataSizeInKB * 1024 / sizeof(double);
+
+        // Generate random data and write to files
+        GenerateRandomDataFile("A.txt", dataSize);
+        GenerateRandomDataFile("B.txt", dataSize);
+        GenerateRandomDataFile("C.txt", dataSize);
+
+        Console.WriteLine("Files A.txt, B.txt, and C.txt have been created with random data.");
+
+        // Creating a Class Instance to Measure Time
+        TimeMeasurement timer = new TimeMeasurement();
+
+        // Measuring Total Execution Time
+        timer.Start();
+
+        // Entering data from files
+        double[] A = ReadArrayFromFile("A.txt");
+        double[] B = ReadArrayFromFile("B.txt");
+        double[] C = ReadArrayFromFile("C.txt");
+        double[] R = CalculateResultArray(A, B, C);
+
+        // Output the results to a file
+        WriteArrayToFile("R.txt", R);
+        timer.Stop();
+
+        double elapsedTime = timer.GetElapsedTime();
+
+        // Measuring CPU Time
+        TimeSpan processTime = timer.GetProcessTime();
+        double elapsedTimeUsingTimeGetTime = timer.GetElapsedTimeUsingTimeGetTime();
+
+        // Screen Time Display
+        Console.WriteLine($"Elapsed Time (QueryPerformanceCounter): {elapsedTime} seconds");
+        Console.WriteLine($"Elapsed Time (timeGetTime): {elapsedTimeUsingTimeGetTime} seconds");
+        Console.WriteLine($"Process Time: {processTime.TotalSeconds} seconds");
+    }
+
+    static void GenerateRandomDataFile(string fileName, int dataSize)
+    {
+        Random random = new Random();
+        using (StreamWriter writer = new StreamWriter(fileName, false)) // Open file with overwrite mode
+        {
+            for (int i = 0; i < dataSize; i++)
+            {
+                double randomValue = random.NextDouble() * 100; // Generate a random double value between 0 and 100
+                writer.WriteLine(randomValue);
+            }
+        }
+    }
+
+    static double[] ReadArrayFromFile(string fileName)
+    {
+        string[] lines = File.ReadAllLines(fileName);
+        double[] array = new double[lines.Length];
+        for (int i = 0; i < lines.Length; i++)
+        {
+            array[i] = double.Parse(lines[i]);
+        }
+        return array;
+    }
+
+    static void WriteArrayToFile(string fileName, double[] array)
+    {
+        using (StreamWriter writer = new StreamWriter(fileName))
+        {
+            foreach (double value in array)
+            {
+                writer.WriteLine(value);
+            }
+        }
+    }
+
+    static double[] CalculateResultArray(double[] A, double[] B, double[] C)
+    {
+        int length = A.Length;
+        double[] R = new double[length];
+        double avgA = CalculateAverage(A);
+        double avgB = CalculateAverage(B);
+        double avgC = CalculateAverage(C);
+
+        for (int i = 0; i < length; i++)
+        {
+            R[i] = A[i] * avgA + B[i] * avgB + C[i] * avgC;
+        }
+
+        return R;
+    }
+
+    static double CalculateAverage(double[] array)
+    {
+        double sum = 0;
+        foreach (double value in array)
+        {
+            sum += value;
+        }
+        return sum / array.Length;
+    }
+}
---
+++ TimeMeasurement.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+public class TimeMeasurement
+{
+    [DllImport("kernel32.dll")]
+    private static extern bool QueryPerformanceCounter(out long lpPerformanceCount);
+
+    [DllImport("kernel32.dll")]
+    private static extern bool QueryPerformanceFrequency(out long lpFrequency);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    private static extern bool GetProcessTimes(IntPtr hProcess, out FILETIME lpCreationTime, out FILETIME lpExitTime, out FILETIME lpKernelTime, out FILETIME lpUserTime);
+
+    [DllImport("kernel32.dll")]
+    private static extern IntPtr GetCurrentProcess();
+
+    [DllImport("winmm.dll", EntryPoint = "timeGetTime")]
+    private static extern uint TimeGetTime();
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct FILETIME
+    {
+        public uint dwLowDateTime;
+        public uint dwHighDateTime;
+    }
+
+    private long _frequency;
+    private long _startCounter;
+    private long _endCounter;
+    private uint _startTimeGetTime;
+    private uint _endTimeGetTime;
+
+    public TimeMeasurement()
+    {
+        if (!QueryPerformanceFrequency(out _frequency))
+        {
+            throw new InvalidOperationException("High-performance counter not supported.");
+        }
+    }
+
+    public void Start()
+    {
+        QueryPerformanceCounter(out _startCounter);
+        _startTimeGetTime = TimeGetTime();
+    }
+
+    public void Stop()
+    {
+        QueryPerformanceCounter(out _endCounter);
+        _endTimeGetTime = TimeGetTime();
+    }
+
+    public double GetElapsedTime()
+    {
+        return (double)(_endCounter - _startCounter) / _frequency;
+    }
+
+    public double GetElapsedTimeUsingTimeGetTime()
+    {
+        return (_endTimeGetTime - _startTimeGetTime) / 1000.0;
+    }
+
+    public TimeSpan GetProcessTime()
+    {
+        GetProcessTimes(GetCurrentProcess(), out _, out _, out FILETIME kernelTime, out FILETIME userTime);
+        ulong kernel = ((ulong)kernelTime.dwHighDateTime << 32) | kernelTime.dwLowDateTime;
+        ulong user = ((ulong)userTime.dwHighDateTime << 32) | userTime.dwLowDateTime;
+        return TimeSpan.FromTicks((long)(kernel + user));
+    }
+}
